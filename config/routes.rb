e074f97b8b194672ea15b9a2423ab183c5ccd174
Nathanial L. McConnell <nate@synaptian.com>
2014-07-23 11:41:54 -0400

    correcting typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :photos, only: [:new, :create, :index]
-  root_to 'photos#index'
-  
+  root to: 'photos#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
